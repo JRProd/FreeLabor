d4c4372fdd69a6eaf6c17771c524fc8cebfbdbe2
EC2 Default User <ec2-user@ip-172-31-18-160.us-west-2.compute.internal>
2016-10-28 06:22:05 +0000

    Init Commit for dev virtualbox

---
+++ config.js
@@ -0,0 +1,16 @@
+var config = {};
+
+config.mysql_prod = {};
+config.mysql_staging = {};
+config.mysql_local = {};
+
+config.mysql_prod.user_name = "app_prod";
+config.mysql_prod.password = "";
+
+config.mysql_staging.user_name = "app_staging";
+config.mysql_staging.password = "";
+
+config.mysql_local.user_name = "app_local"
+config.mysql_local.password = "sunshine1"
+
+module.exports = config;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
+var config = require('./config.js'); //Will be used to store server user config files
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -16,6 +17,7 @@ router.get('/', function(req, res) {
 //put more routes here
 
 //Register Routes
+//app.use(express.static(__dirname + '/FrontEnd'));
 app.use('/', router);
 
 app.listen(port);
