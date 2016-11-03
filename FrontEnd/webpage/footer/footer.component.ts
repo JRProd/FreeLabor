0af47e4ffbfcb46620c89def7b40da69d941d63e
Jake Rowland <jakerowland@verizon.net>
2016-11-02 21:20:08 -0500

    Added the footer implmentation

--- FrontEnd/webpage/footer/footer.component.ts
+++ FrontEnd/webpage/footer/footer.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'footer',
-    templateUrl: '/webpage/footer/footer.html',
-    styleUrls: ['/webpage/footer/footer.css' ],
+    templateUrl: './webpage/footer/footer.html',
+    styleUrls: ['./webpage/footer/footer.css' ],
 })
 
 export class Footer
