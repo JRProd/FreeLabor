860e96a06e1a94fdcac95c297c1cbb157f2a8f7c
Luke Hansen <respun1000@gmail.com>
2016-11-03 17:23:19 -0500

    meh

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 var express    = require('express');
-var app        = express();
+var mysql      = require('mysql');
 var bodyParser = require('body-parser');
-var config = require('./config.js'); //Will be used to store server user config files
+var path       = require('path');
+var config     = require('./config.js'); //prod sql auth will go here
+var app        = expxress();
+var router = express.Router();
+var port = process.env.PORT || 8080;
+
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : config.mysql_local.user_name,
+  password : config.mysql_local.password
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;
-var router = express.Router();
-
 //Test route for homepage
-app.use(express.static(__dirname+'/FrontEnd'))
-app.use(require('./routes/orgs'));
+app.use(express.static(path.join(__dirname,'FrontEnd'))); //FrontEnd/public
+
+app.use('/',function(req,res){
+  res.sendFile('index.html');
+});
+//app.use(require('./routes/orgs'));
 //app.use(require('./routes/events'));
 //app.use(require('./routes/users'));
 
@@ -20,7 +40,7 @@ app.use(require('./routes/orgs'));
 
 //Register Routes
 //app.use(express.static(__dirname + '/FrontEnd'));
-app.use('/', router);
+//app.use('/', router);
 
 app.listen(port);
 console.log('FreeLabor API Serving on port ' + port);
