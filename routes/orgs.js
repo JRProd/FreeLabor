ffe1931eb3363173af3d46e147d20bdb6b220f1f
AJD- <ajdetorrice@hotmail.com>
2016-11-07 14:52:43 -0600

    added sql connection to post, still needs correct username/password

--- routes/orgs.js
+++ routes/orgs.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var responses = require('./responses.js');
+var connection = mysql.createConnection({
+	host		: 'localhost',
+	user		: 'app_dev',
+	password	: 'password',
+	database	: 'freelabor'
+});
 
 router.get('/orgs/:username', function(req,res){
   //server logic to fetch "all" info about 
@@ -15,6 +21,8 @@ router.post('/orgs', function(request,response){
   //v
   res.send("Default response");
   //Server logic to create a user in the database
+  var post = {};
+  connection.query('INSERT INTO Org VALUES ?', post, function(err, result){});
 });
 
 
