0441b230d78e034fbb434c8410539cdaf17a08b4
Lucas Hansen <respun1000@gmail.com>
2016-11-07 23:37:41 -0600

    Added user create (POST) and retrieval (GET)

--- routes/orgs.js
+++ routes/orgs.js
@@ -1,12 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var responses = require('./responses.js');
-var connection = mysql.createConnection({
-	host		: 'localhost',
-	user		: 'app_dev',
-	password	: 'password',
-	database	: 'freelabor'
-});
 
 router.get('/orgs/:username', function(req,res){
   //server logic to fetch "all" info about 
@@ -21,8 +15,6 @@ router.post('/orgs', function(request,response){
   //v
   res.send("Default response");
   //Server logic to create a user in the database
-  var post = {};
-  connection.query('INSERT INTO Org VALUES ?', post, function(err, result){});
 });
 
 
--- server.js
+++ server.js
@@ -7,29 +7,29 @@ var config     = require('./config.js'); //prod sql auth will go here
 var session = require('express-session');
 var MySQLServerStore = require('express-mysql-session')(session);
 //end of session-specific requires
-var app        = expxress();
+var app        = express();
 var router = express.Router();
 var port = process.env.PORT || 8080;
 
 //options for sessions, need to change user/password for the database
 var options = {
-    host: 'localhost',
-    port: 3306,
-    user: config.mysql_local.user_name,
-    password: config.mysql_local.password,
-    database: 'freelabor',// Database name. 
-    checkExpirationInterval: 900000,// How frequently expired sessions will be cleared; milliseconds. 
-    expiration: 86400000,// The maximum age of a valid session; milliseconds. 
-    createDatabaseTable: true,// Whether or not to create the sessions database table, if one does not already exist. 
-    connectionLimit: 1,// Number of connections when creating a connection pool 
-    schema: {
-        tableName: 'sessions',
-        columnNames: {
-            session_id: 'session_id',
-            expires: 'expires',
-            data: 'userID'
-        }
-    }
+	host: 'localhost',
+	port: 3306,
+	user: config.mysql_local.user_name,
+	password: config.mysql_local.password,
+	database: 'freelabor',// Database name.
+	checkExpirationInterval: 900000,// How frequently expired sessions will be cleared; milliseconds.
+	expiration: 86400000,// The maximum age of a valid session; milliseconds.
+	createDatabaseTable: true,// Whether or not to create the sessions database table, if one does not already exist.
+	connectionLimit: 1,// Number of connections when creating a connection pool
+	schema: {
+		tableName: 'Session',
+		columnNames: {
+			session_id: 'idSession',
+			expires: 'expiresSession',
+			data: 'idUser'
+		}
+	}
 };
 
 //using express-mysql-session for storing sessions
@@ -37,8 +37,9 @@ var sessionStore = new MySQLServerStore(options);
 
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : config.mysql_local.user_name,
-  password : config.mysql_local.password
+  user     : 'app_dev',
+  password : 'sunshine1',
+  database : 'freelabor'
 });
 
 connection.connect(function(err) {
@@ -49,18 +50,26 @@ connection.connect(function(err) {
   console.log('connected as id ' + connection.threadId);
 });
 
+app.all('*', function(request, response, next){
+  request.db = connection;
+	request.ss = sessionStore;
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //Test route for homepage
 app.use(express.static(path.join(__dirname,'FrontEnd'))); //FrontEnd/public
-
-app.use('/',function(req,res){
-  res.sendFile('index.html');
-});
+app.use(require('./routes/users'));
 //app.use(require('./routes/orgs'));
 //app.use(require('./routes/events'));
-//app.use(require('./routes/users'));
+
+app.use('/',function(req,res){
+  //res.sendFile('index.html');
+
+});
+
 
 
 //put more routes here
