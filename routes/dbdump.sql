08f6f8bf81f641a0ee86cde01790e7955b4f03f7
Lucas Hansen <respun1000@gmail.com>
2016-11-17 18:03:18 -0600

    fixed dump

--- routes/dbdump.sql
+++ routes/dbdump.sql
@@ -1,6 +1,23 @@
-CREATE DATABASE  IF NOT EXISTS `freelabor` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `freelabor`;
+-- MySQL dump 10.13  Distrib 5.7.12, for osx10.9 (x86_64)
+--
+-- Host: localhost    Database: freelabor
+-- ------------------------------------------------------
+-- Server version	5.7.16
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Attendance`
+--
 
 DROP TABLE IF EXISTS `Attendance`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -18,12 +35,9 @@ CREATE TABLE `Attendance` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-
-LOCK TABLES `Attendance` WRITE;
-/*!40000 ALTER TABLE `Attendance` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Attendance` ENABLE KEYS */;
-UNLOCK TABLES;
-
+--
+-- Table structure for table `Event`
+--
 
 DROP TABLE IF EXISTS `Event`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -45,13 +59,9 @@ CREATE TABLE `Event` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-
-LOCK TABLES `Event` WRITE;
-/*!40000 ALTER TABLE `Event` DISABLE KEYS */;
-INSERT INTO `Event` VALUES (1,2,'title','location','123 address ln.','Dallas','TX','75275','2016-11-09T12:00:00+00:00','2016-11-09T18:00:00+00:00','insert a rather lengthy description on the event here',100),(2,2,'title','location','123 address ln.','Dallas','TX','75275','2016-11-09T12:00:00+00:00','2016-11-09T18:00:00+00:00','insert a rather lengthy description on the event here',100);
-/*!40000 ALTER TABLE `Event` ENABLE KEYS */;
-UNLOCK TABLES;
-
+--
+-- Table structure for table `Membership`
+--
 
 DROP TABLE IF EXISTS `Membership`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -69,12 +79,9 @@ CREATE TABLE `Membership` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-
-LOCK TABLES `Membership` WRITE;
-/*!40000 ALTER TABLE `Membership` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Membership` ENABLE KEYS */;
-UNLOCK TABLES;
-
+--
+-- Table structure for table `Org`
+--
 
 DROP TABLE IF EXISTS `Org`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -94,13 +101,9 @@ CREATE TABLE `Org` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-
-LOCK TABLES `Org` WRITE;
-/*!40000 ALTER TABLE `Org` DISABLE KEYS */;
-INSERT INTO `Org` VALUES (1,'Test Organization','test','test@org.org','12174235546','AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
-/*!40000 ALTER TABLE `Org` ENABLE KEYS */;
-UNLOCK TABLES;
-
+--
+-- Table structure for table `Session`
+--
 
 DROP TABLE IF EXISTS `Session`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -113,12 +116,9 @@ CREATE TABLE `Session` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-
-LOCK TABLES `Session` WRITE;
-/*!40000 ALTER TABLE `Session` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Session` ENABLE KEYS */;
-UNLOCK TABLES;
-
+--
+-- Table structure for table `User`
+--
 
 DROP TABLE IF EXISTS `User`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -136,13 +136,6 @@ CREATE TABLE `User` (
   UNIQUE KEY `usernameUser_UNIQUE` (`usernameUser`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
-
-LOCK TABLES `User` WRITE;
-/*!40000 ALTER TABLE `User` DISABLE KEYS */;
-INSERT INTO `User` VALUES (6,'Francis','XXXMLCoyle','json_sucks3@rollo.biz','rollo3','$2a$10$fhY.UY/fr4CN78qrO6BqUeHb1zUIuMY8iOhNItOc6K4KaGTXEjjvS');
-/*!40000 ALTER TABLE `User` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -152,3 +145,5 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-11-17 17:53:50
