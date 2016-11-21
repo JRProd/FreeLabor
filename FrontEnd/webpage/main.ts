e50eeb9cb054f7b082d2369740b4d6ba677ef401
EttaMari <emgodwin@smu.edu>
2016-11-21 15:44:26 -0600

    removed webpage folder

--- FrontEnd/webpage/main.ts
+++
@@ -1,5 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';  
-import { AppModule } from './webpage.module';    
-
-const platform = platformBrowserDynamic();  
-platform.bootstrapModule(AppModule);  
--- FrontEnd/webpage/webpage.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'webpage',
-    templateUrl: './webpage/webpage.html',
-})
-export class WebPageComponent{
-
-}
