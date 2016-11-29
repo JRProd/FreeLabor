ea160b7d8633d438ff06e79d4559b8c2b13a6ad2
Lucas Hansen <respun1000@gmail.com>
2016-11-29 12:41:50 -0600

    ?in this commit?

--- routes/events.js
+++ routes/events.js
@@ -30,7 +30,7 @@ router.post('/event', function(req,res){
 			res.json({success:false,message:err});
 		} else {
 			//Right now I have no way to get the org's name
-			res.json({success:true,message:rows,url:'http://localhost/org/'+req.body.username + '/events/' + req.body.title});
+			res.json({success:true,message:rows,url:'http://localhost/org/'+req.body.username + '/events/' + rows.insertId});
 		}
 	});
 
@@ -38,43 +38,50 @@ router.post('/event', function(req,res){
 
 
 //Copy and paste code from orgs.js for reference
-router.get('/event/:usernameOrg/:idEvent', function(req,res){
-	var sql = 'SELECT title,address,city,state,zip,dateStart,dateEnd,description,maxAttendees FROM Event WHERE eventID=?';
-	var params = [req.params.eventID];
-	function performQuery(req,query,data,callback) {
-		req.db.query(query, data, function(err, rows, fields) {
-			if (err) {
-				callback(err, null);
-			} else
-				callback(null, rows, fields);
-	  });
-	}
+router.get('/org/:username/events/:idEvent', function(req,res){
+
+	var sql = 'SELECT * FROM Event WHERE idEvent=?';
+	var params = [req.params.idEvent];
+
 	performQuery(req,sql, params, function(err, rows, fields) {
 		if (err) {
 			res.json({success:false,message:err});
 		} else {
-			var responseObj = responses.getOrg;
 			var toAdd = {
 				success:true,
-				title:rows[0].title,
-				address:rows[0].address,
-				city:rows[0].city,
-				state:rows[0].state,
-				zip:rows[0].zip,
-				dateStart:rows[0].dateStart,
-				dateEnd:rows[0].dateEnd,
-				description:rows[0].description,
-				maxAttendees:rows[0].maxAttendees
+				title:rows[0].titleEvent,
+				address:rows[0].addressEvent,
+				city:rows[0].cityEvent,
+				state:rows[0].stateCodeEvent,
+				zip:rows[0].postCodeEvent,
+				dateStart:rows[0].dateStartEvent,
+				dateEnd:rows[0].dateEndEvent,
+				description:rows[0].descriptionEvent,
+				maxAttendees:rows[0].maxAttendeesEvent,
+				condensedVolunteers:null
 			};
-			responseObj = Object.assign(responseObj,toAdd);
-			res.json(responseObj);
+			res.json(toAdd);
 		}
 	});
 
 });
 //Modify live event
-router.put('/event/:usernameOrg/:idEvent', function(req,res){
+router.patch('/org/:username/events/:idEvent', function(req,res){
 
+  if(req.body.attendee){
+    //TODO: if it contains a username, we will need to add to the "membership"
+    var insert = 'INSERT INTO Attendance(idUser,idEvent,dateJoinedAttendance) VALUES ((SELECT idUser FROM User WHERE usernameUser=?),?,?)';
+    var date = new Date();
+    var qparams = [req.body.attendee,req.params.idEvent,date.toISOString()];
+    console.log(mysql.format(insert,qparams));
+    performQuery(req,insert,qparams, function(err, rows,fields) {
+      if (err) {
+        res.json({success:false,message:err});
+      } else {
+        res.json({success:true,message:rows,url:'http://localhost/user/'+req.params.username});
+      }
+    });
+  }
 
 });
 
